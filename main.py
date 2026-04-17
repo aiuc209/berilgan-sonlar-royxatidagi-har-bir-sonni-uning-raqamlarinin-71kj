f04215e1f30d6127ac00cfd4e32764de5eb88648
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-18 00:36:34 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Berilgan sonlar ro‘yxatidagi har bir sonni uning raqamlarining o‘rtacha qiymati bilan almashtiring.")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
